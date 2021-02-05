6f3a382f61de9979ae9c322ca8086dc93df411ed
realmadsci <71108352+realmadsci@users.noreply.github.com>
2021-02-04 20:43:59 -0500

    Key generation is complete
    NOTE: It is generally not secure to upload private keys into
          a public git repository. This one is encrypted with a
          128-character random password, so it is *fairly* secure,
          but don't duplicate this example in the real world!

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # guess-that-password
 Password hash reversing challenge, complete with cryptographically signed assignments for automatic grading.
 
-Use generate_keys.py to create a new key pair.
+## Dependencies
+'''
+python -m pip install cryptography
+'''
+
+## Usage
+
+Use generate_keys.py to create a new key pair for your own testing.
+The key pair that is uploade to the server was used for a class assignment, and is protected by a 128-character random password.
+This is fairly secure, but in the real world, you shouldn't actually upload even an encrypted private key into a git repository!
--- generate_keys.py
+++ generate_keys.py
@@ -4,39 +4,29 @@ from pathlib import Path
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ed25519
 
-key_folder = Path('.keys')
+key_folder = Path('keys')
 key_folder.mkdir(exist_ok=True)
 pub_key_path  = key_folder / 'id_ed25519.pub'
 priv_key_path = key_folder / 'id_ed25519'
 
-password = input('Enter private key password: ').encode()
+password = input('Enter signing password: ').encode()
+password2 = input('Repeat signing password: ').encode()
+if password != password2:
+    print('Passwords did not match!')
+    quit()
+
+# Generate the private key
 private_key = ed25519.Ed25519PrivateKey.generate()
 
+# Encrypt and save the private key
 priv_key_path.write_bytes(
     private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.BestAvailableEncryption(password)
-    )
-)
+        encryption_algorithm=serialization.BestAvailableEncryption(password)))
 
+# Save the public key
 pub_key_path.write_bytes(
     private_key.public_key().public_bytes(
         encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-)
-
-
-read_private_bytes = Path('.keys/id_ed25519').read_bytes()
-
-private_key_readback = serialization.load_pem_private_key(
-    read_private_bytes,
-    password=password,
-)
-
-#private_key_readback = ed25519.Ed25519PrivateKey.from_private_bytes(private_bytes2)
-sig1 = private_key.sign(b'hello')
-sig2 = private_key_readback.sign(b'hello')
-
-print(f"they are equal: {sig1 == sig2}")
+        format=serialization.PublicFormat.SubjectPublicKeyInfo))
